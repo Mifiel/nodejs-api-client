8d66505e7247207592f41dae72365be32047c399
Genaro Madrid <genmadrid@gmail.com>
2018-01-23 21:52:02 -0600

    finish authentication

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,27 @@
 export default class Config {
-  private static url: string;
+  private static uri: string;
   private static appId: string;
   private static secret: string;
 
   static setTokens(appId: string, secret: string) {
     this.appId = appId;
     this.secret = secret;
-    this.url = 'https://www.mifiel.com/api/v1'
+    this.uri = 'https://www.mifiel.com/api/v1'
   }
 
-  static setUrl(url: string) {
-    this.url = url;
+  static get appID(): string {
+    return this.appId
   }
 
-  static getUrl(): string {
-    return this.url;
+  static get appSecret(): string {
+    return this.secret;
+  }
+
+  static set url(url: string) {
+    this.uri = url;
+  }
+
+  static get url(): string {
+    return this.uri;
   }
 }
--- src/connection.ts
+++ src/connection.ts
@@ -1,4 +1,7 @@
 import * as request from 'request'
+import * as Crypto from 'crypto'
+
+import ApiAuth from './api-auth'
 import Config from './config'
 
 export enum TYPES {
@@ -14,30 +17,41 @@ export class Connection {
     return new Promise((resolve, reject) => {
       const options: request.Options = {
         url: path,
-        baseUrl: Config.getUrl(),
-        method: 'POST'
+        baseUrl: Config.url,
+        method: 'POST',
+        callback: (error, response, body) => {
+          if (error) {
+            return reject(error)
+          }
+          if (response.statusCode >= 200 && response.statusCode < 300) {
+            return resolve(this.parseBody(body))
+          }
+          reject({
+            status: {
+              code: response.statusCode,
+              message: response.statusMessage
+            },
+            error: this.parseBody(body)
+          })
+        }
       }
       if (type === TYPES.multipart) {
         options.formData = payload
       } else {
         options.json = payload
       }
-      request(options, (error, response, body) => {
-        if (error) {
-          return reject(error)
-        }
-        if (response.statusCode >= 200 && response.statusCode < 300) {
-          resolve(JSON.parse(body))
-        } else {
-          reject({
-            status: {
-              code: response.statusCode,
-              message: response.statusMessage
-            },
-            error: JSON.parse(body)
-          })
-        }
-      })
+      const req = request(options)
+      const apiAuth = new ApiAuth(req)
+      apiAuth.init(Config.appID, Config.appSecret)
+      apiAuth.onRequest()
     })
+  }
+
+  private static parseBody(body: any): Object {
+    try {
+      return JSON.parse(body)
+    } catch (e) {
+      return body
+    }
   }
 }
