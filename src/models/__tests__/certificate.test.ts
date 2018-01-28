b318243a4b20055dbfb0b6a74fbc948ba3424405
Genaro Madrid <genmadrid@gmail.com>
2018-01-27 23:16:02 -0600

    test certificate

--- src/models/__tests__/certificate.test.ts
+++ src/models/__tests__/certificate.test.ts
@@ -2,16 +2,32 @@ import { Connection } from '../../connection';
 import Mifiel from '../../'
 
 jest.mock('../../connection')
+const { Certificate } = Mifiel.Models
 
-describe('save certificate', () => {
-  it('should return 201 CREATED', () => {
-    const cert = new Mifiel.Models.Certificate({
-      file: './test/fixtures/FIEL_FISICA.cer'
+describe('create a certificate', () => {
+  describe('with a file', () => {
+    it('should return 201 CREATED', () => {
+      const cer = new Certificate({
+        file: './test/fixtures/FIEL_FISICA.cer'
+      })
+      const spy = jest.spyOn(Connection, 'post')
+      expect(cer.save()).resolves.toHaveProperty('cer_hex')
+      expect(spy).toHaveBeenCalledWith(
+        'keys',
+        expect.objectContaining({
+          cer_file: expect.anything()
+        }),
+        1
+      )
     })
-    cert.save().then(resp => {
-      expect(resp).toHaveProperty('cer_hex')
-    }).catch(err => {
-      console.error(err)
+  })
+
+  describe('with unexistant file', () => {
+    it('should throw an error', () => {
+      const cer = new Certificate({
+        file: 'bad-certificate'
+      })
+      expect(cer.save).toThrowError()
     })
   })
 })
