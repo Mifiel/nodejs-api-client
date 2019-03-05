885e7383fb630b91f4d17425033ba4de3bf8b06f
aldo <aldosolorzanop@gmail.com>
2019-03-05 17:15:19 -0600

    Simple tests for ECIES due to lack of suppor in Jest

--- src/crypto/__tests__/ecies.test.ts
+++ src/crypto/__tests__/ecies.test.ts
@@ -1,34 +1,29 @@
 import ECIES from '../ecies';
 import * as fs from 'fs'
 import { util } from 'node-forge'
-import * as ecc from 'eccrypto'
 import * as crypto from 'crypto'
 
 const eciesFixture = JSON.parse(fs.readFileSync('test/fixtures/ecies.json', 'utf8'));
+const failure = false
+console.log('#Valid data');
+console.log('\tvalid ECIES encrypt & decrypt')
 
-describe('#Valid data', () => {
-	describe('valid ECIES encrypt & decrypt', () => {
-		// eciesFixture.valid.forEach((item) => {
-			const item = eciesFixture.valid[0]
-			describe(JSON.stringify(Object.assign({}, item.encryptedData, item.decrypted)), () => {
-				const { privateKey, decrypted, encryptedData, publicKeyUncompress } = item
-				it('Should encrypt data', async () => {
-					return ECIES.encrypt(Buffer.from(item.publicKeyUncompress, 'hex'), decrypted).then(function(encrypted) {
-					  // B decrypting the message.
-					  return ECIES.decrypt(Buffer.from(privateKey, 'hex'), encrypted).then(function(plaintext) {
-					    console.log("Message to part B:", plaintext.toString());
-					  });
-					});
-				// 	const priv = crypto.randomBytes(32);
-				// 	// const encrypted  = await ECIES.encrypt(Buffer.from(publicKeyUncompress, 'hex'), decrypted);
-				// 	// const plaintext = await ECIES.decrypt(Buffer.from(privateKey, 'hex'), encrypted);
-				// 	// expect(plaintext.toString()).toEqual(decrypted)
-				})
-				// it('Should decrypt data', async () => {
-				// 	const plainText = await ECIES.decrypt(Buffer.from(privateKey,'hex'), ECIES.toBuffer(encryptedData))
-				// 	expect(plainText.toString()).toEqual(decrypted)
-				// })
-			})
-		// })
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+const beginTest = async () => {
+	await asyncForEach(eciesFixture.valid, async (item)=> {
+		const { privateKey, decrypted, encryptedData, publicKeyUncompress } = item
+		console.log('\tShould encrypt data');
+			const encrypted  = await ECIES.encrypt(Buffer.from(publicKeyUncompress, 'hex'), decrypted);
+			const plaintext = await ECIES.decrypt(Buffer.from(privateKey, 'hex'), encrypted);
+			console.log('\t\t' + (plaintext.toString() == decrypted))
+		console.log('\tShould decrypt data')
+			const plainText = await ECIES.decrypt(Buffer.from(privateKey,'hex'), ECIES.toBuffer(encryptedData))
+			console.log('\t\t'+ (plainText.toString() == decrypted))
 	})
-})
+}
+beginTest()
